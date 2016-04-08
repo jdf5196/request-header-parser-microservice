806e2f4425bcae574c5d197f7069cea19340fb87
Joshua Francona <jfrancona87@gmail.com>
2016-04-08 14:22:12 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const express = require('express');
+const ip = require('ip');
+const app = express();
+
+let port = (process.env.PORT || 5000);
+
+app.set('port', port);
+
+app.use(express.static(process.cwd() + '/public'));
+
+app.get('/whatami', function(req, res){
+	console.log(req.socket.remoteAddress);
+	res.json({
+		'IP Address': req.socket.remoteAddress,
+		'Language': req.headers["accept-language"].split(",")[0],
+		'Software': req.headers["user-agent"].match(/\((.*?)\)/)[1]
+	})
+});
+
+app.listen(app.get('port'), function(){
+	console.log('Server listening on port ' + port);
+})
